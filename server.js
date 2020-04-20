19bc34e3aa5abfb47d2eac5ea6bc2dd244ff1285
hervengenzi <ngenziherve@octagrp.com>
2020-04-20 13:18:10 +0200

    Add required packages for file and picture upload

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
+
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const _ = require('lodash')
 
 var mongoose = require('mongoose')
 
@@ -12,6 +17,15 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.use(cors());
+app.use(bodyParser().json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const taskRouter = require('./routes/tasks')
 app.use('/tasks', taskRouter)
