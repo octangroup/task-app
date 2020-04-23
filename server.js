a08bd696e5b5e068ebb485f34f4d253fddaeedb6
hervengenzi <ngenziherve@octagrp.com>
2020-04-23 09:28:56 +0300

    User file upload

--- server.js
+++ server.js
@@ -18,14 +18,52 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
+// to access the uploaded file publicly from anywhere
+
+app.use(express.static('uploads'));
+
+// file upload middleware
+
 app.use(fileUpload({
-    createParentPath: true
+    createParentPath: true,
+    limits: { 
+        fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
+    },
 }));
 
 app.use(cors());
-app.use(bodyParser().json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+app.post('/upload-avatar', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 const taskRouter = require('./routes/tasks')
 app.use('/tasks', taskRouter)
